ca163b1c05ab68c3c3d56cdfd45854ae74af07e5
Ethan Holland <ethanjholland@gmail.com>
2019-10-07 17:06:14 -0400

    working create tables

--- milestone1/create-tables.sql
+++ milestone1/create-tables.sql
@@ -36,23 +36,23 @@ arrival_datetime DATE NOT NULL,
 duration INTEGER NOT NULL CHECK (duration > 0),
 mileage INTEGER NOT NULL CHECK (mileage > 0));
 
-INSERT INTO Users VALUES (0, 'sqllover@duke.edu', 'encryptedpassword');
-INSERT INTO Users VALUES (1, 'ra4ever@duke.edu', 'otherpassword');
+INSERT INTO Users VALUES (0, 'sqllover@duke.edu', crypt('encryptedpassword', gen_salt('bf')));
+INSERT INTO Users VALUES (1, 'ra4ever@duke.edu', crypt('otherpassword', gen_salt('bf')));
 
 INSERT INTO Trips VALUES(0, 0, 'to internship');
 INSERT INTO Trips VALUES(1, 1, 'graduation');
-INSERT INTO Trips VALUES(0, 2, 'from internship');
+INSERT INTO Trips VALUES(2, 0, 'from internship');
 
 
-INSERT INTO Airlines VALUES('DAL', 'Delta Air Lines');
-INSERT INTO Airlines VALUES ('ASA', 'Alaska Airlines');
+INSERT INTO Airlines VALUES('DL', 'Delta Air Lines');
+INSERT INTO Airlines VALUES ('AS', 'Alaska Airlines');
 
 INSERT INTO Airports VALUES ('RDU', 'Raleigh Durham International Airport', 'Raleigh-Durham', 'USA', 35.8776, -78.7875, 'America/ New_York', 'A');
 INSERT INTO Airports VALUES ('IAD', 'Washington Dulles International Airport', 'Washington, DC', 'USA', 38.9445, -77.4558, 'America/ New_York', 'A');
 INSERT INTO Airports VALUES ('SEA', 'Seattle Tacoma International Airport', 'Seattle', 'USA', 47.4490, -122.309, 'America/ Los_Angeles', 'A');
 INSERT INTO Airports VALUES ('BWI', 'Baltimore/Washington International Thurgood Marshall Airport', 'Baltimore', 'USA', 39.1754, -76.6683, 'America/ New_York', 'A');
 
-INSERT INTO Flights VALUES(3, 2, 'ASA', 0922, 'SEA', 'BWI', '20190727 08:00:00 AM', '20190727 4:15:00 PM', 315, 2335); 
-INSERT INTO Flights VALUES(2, 1, 'DAL', 2220, 'IAD', 'SEA', '20190609 05:00:00 PM', '20190609 07:39:00 PM', 339, 2306); 
-INSERT INTO Flights VALUES(1, 1, 'DAL', 2680, 'SEA', 'IAD', '20190605 10:45:00 PM', '20190606 06:07:00 AM', 339, 2306); 
-INSERT INTO Flights VALUES(0, 0, 'DAL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355); 
+INSERT INTO Flights VALUES(3, 2, 'AS', 0922, 'SEA', 'BWI', '20190727 08:00:00 AM', '20190727 4:15:00 PM', 315, 2335); 
+INSERT INTO Flights VALUES(2, 1, 'DL', 2220, 'IAD', 'SEA', '20190609 05:00:00 PM', '20190609 07:39:00 PM', 339, 2306); 
+INSERT INTO Flights VALUES(1, 1, 'DL', 2680, 'SEA', 'IAD', '20190605 10:45:00 PM', '20190606 06:07:00 AM', 339, 2306); 
+INSERT INTO Flights VALUES(0, 0, 'DL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355); 
