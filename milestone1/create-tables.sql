cb2c61ac6cf8e4d6b88993e3bb123a2275c99be3
Jaiveer Katariya <jk386@duke.edu>
2019-10-07 16:09:25 -0400

    working on insert statements

--- milestone1/create-tables.sql
+++ milestone1/create-tables.sql
@@ -5,8 +5,8 @@ flight_airline CHAR(3) REFERENCES Airlines.iata_code,
 flight_num INTEGER,
 depart_aid CHAR(3) NOT NULL REFERENCES Airports.iata_code,
 arrival_aid CHAR(4) NOT NULL REFERENCES Airports.iata_code,
-depart_date DATE NOT NULL,
-arrival_date DATE NOT NULL,
+depart_datetime DATE NOT NULL,
+arrival_datetime DATE NOT NULL,
 duration INTEGER NOT NULL,
 mileage INTEGER NOT NULL);
 
@@ -22,7 +22,7 @@ country VARCHAR(50) NOT NULL,
 longitude FLOAT NOT NULL, 
 latitude FLOAT NOT NULL, 
 time_zone VARCHAR(5) NOT NULL,
-dst CHAR(1) NOT NULL);
+dst CHAR(1) NOT NULL IN ('E', 'A', 'S', 'O', 'Z', 'N', 'U'));
 
 CREATE TABLE Users
 (uid INTEGER NOT NULL PRIMARY KEY, 
@@ -33,3 +33,13 @@ CREATE TABLE Trips
 (tid INTEGER NOT NULL PRIMARY KEY,
 uid REFERENCES Users.uid,
 trip_name VARCHAR(30));
+
+
+INSERT INTO USER VALUES (0, 'sqllover@duke.edu', 'encryptedpassword');
+INSERT INTO USER VALUES (1, 'ra4ever@duke.edu', 'otherpassword');
+
+INSERT INTO FLIGHTS VALUES(3, 2, 'ASA', 0922, 'SEA', 'BWI', '20190727 8:00:00 AM', '20190727 4:15:00 PM', 315, 2335); 
+
+
+
+
