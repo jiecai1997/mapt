5e672f649dafa56dabac3ae417a6620e4030f57d
Ethan Holland <ethanjholland@gmail.com>
2019-10-23 17:17:03 -0400

    simplified file structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
-import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import {environment} from "../environments/environment";
+import { AppComponent } from '@app/containers/app';
+import { MapComponent } from '@app/map/map.component';
+import { environment } from "@app/../environments/environment";
 
 let apiKey: string = environment.apiKey;
 
---
+++ frontend/src/app/containers/app.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<div main><app-map></app-map></div>'
+})
+
+export class AppComponent {}
