e18620643a240456b462f5450f8b5b6c115df694
Jaiveer Katariya <jk386@duke.edu>
2019-10-23 23:00:08 -0400

    @app stuff wasn't working for me so just making sure I don't push broken code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { environment } from "@app/../environments/environment";
+import { environment } from "../environments/environment";
 
 //containers
-import { AppComponent } from '@app/app';
-import { HomepageComponent } from '@app/homepage/homepage.component';
+import { AppComponent } from './app';
+import { HomepageComponent } from './homepage/homepage.component';
 
 //components
-import { MapComponent } from '@app/map/map.component';
-import { EdittripComponent } from '@app/edittrip/edittrip.component';
-import { TopbarComponent } from '@app/topbar/topbar.component';
+import { MapComponent } from './map/map.component';
+import { EdittripComponent } from './edittrip/edittrip.component';
+import { TopbarComponent } from './topbar/topbar.component';
 import { StatsComponent } from './stats/stats.component';
 
 let apiKey: string = environment.apiKey;
