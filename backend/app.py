803255b5f01f6810c0235eef8bd02117d5b4a50c
Eric Tay <eric.tay@duke.edu>
2019-10-30 18:03:08 -0400

    config file

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@ import sqlite3
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('database.db')
-print "Opened database successfully";
-
-conn.execute('CREATE TABLE users (uid INT, username TEXT, email TEXT, password TEXT, public BOOLEAN)')
-print "Table created successfully";
-conn.close()
+# conn = sqlite3.connect('database.db')
+# print "Opened database successfully";
+#
+# conn.execute('CREATE TABLE users (uid INT, username TEXT, email TEXT, password TEXT, public BOOLEAN)')
+# print "Table created successfully";
+# conn.close()
 
 if __name__ == '__main__':
 	app.run(debug=True)
