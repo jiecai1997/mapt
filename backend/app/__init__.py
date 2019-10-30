f2eda8c951242ba2ab1b0de2d5799e100c8e4963
jiecai1997 <jiecai1997@gmail.com>
2019-10-30 18:28:34 -0400

    login updates pt 1

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from config import Config
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-from app import routes
+db = SQLALchemy(app)
+migarte = Migrate(app,db)
+
+
+from app import routes, models
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,20 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 import sqlite3 as sql
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('/'))
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/list')
 def list():
