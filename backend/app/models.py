2a90ab49a559117aa73c353b688c1fb0cb4b6ec8
Eric Tay <eric.tay@duke.edu>
2019-10-30 21:10:55 -0400

    Added Trips To HTML

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import validates
 from sqlalchemy import CheckConstraint
 
 class User(db.Model):
-	__tablename__ = 'address'
-
 	uid = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(50), unique=True, nullable=False)
@@ -18,7 +16,7 @@ class User(db.Model):
 
 class Trips(db.Model):
 	tid = db.Column(db.Integer, primary_key=True)
-	uid = db.Column(db.Integer, db.ForeignKey('User.uid'), nullable=False)
+	uid = db.Column(db.Integer, db.ForeignKey(User.uid), nullable=False)
 	trip_name = db.Column(db.String(30), default="Trip Created on " + str(datetime.now()), nullable=False)
 
 	def __repr__(self):
@@ -53,11 +51,11 @@ class Airports(db.Model):
 
 class Flights(db.Model):
 	fid = db.Column(db.Integer, primary_key=True)
-	tid = db.Column(db.Integer, db.ForeignKey('Trips.tid'), nullable=False)
-	airline_iata = db.Column(db.String(2), db.ForeignKey('Airlines.iata'))
+	tid = db.Column(db.Integer, db.ForeignKey(Trips.tid), nullable=False)
+	airline_iata = db.Column(db.String(2), db.ForeignKey(Airlines.iata))
 	flight_num = db.Column(db.Integer) ##TODO: does this need to be >0?
-	depart_iata = db.Column(db.String(3), db.ForeignKey('Airports.iata'), nullable=False)
-	arrival_iata = db.Column(db.String(3), db.ForeignKey('Airports.iata'), nullable=False)
+	depart_iata = db.Column(db.String(3), db.ForeignKey(Airports.iata), nullable=False)
+	arrival_iata = db.Column(db.String(3), db.ForeignKey(Airports.iata), nullable=False)
 	depart_datetime = db.Column(db.DateTime, nullable=False)
 	arrival_datetime = db.Column(db.DateTime, nullable=False)
 	duration = db.Column(db.Integer, nullable=False)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -30,19 +30,23 @@ def list():
 	with sql.connect("app.db") as con:
 		con.row_factory = sql.Row
 		cur = con.cursor()
-		#cur.execute('CREATE TABLE IF NOT EXISTS users (uid INT, username TEXT, email TEXT, password TEXT, public BOOLEAN)')
 		cur.execute("select * from user")
-		rows = cur.fetchall()
+		userrows = cur.fetchall()
 		cur.close()
-	return render_template("list.html",rows = rows)
+		cur = con.cursor()
+		cur.execute("select * from trips")
+		tripsrows = cur.fetchall()
+		cur.close()
+	return render_template("list.html",userrows = userrows,tripsrows = tripsrows)
 
 @app.route('/update')
 def update():
 	with sql.connect("app.db") as con:
 		con.row_factory = sql.Row
 		cur = con.cursor()
-		cur.execute("INSERT INTO user(uid, username, email, password, public) VALUES(1, 'llama', 'llama@gmail.com', 'llamallamallama', 1)")
-		cur.execute("INSERT INTO user(uid, username, email, password, public) VALUES(2, 'alpaca', 'alpaca@gmail.com', 'alpacaalpacaalpaca', 1)")
+		#cur.execute("INSERT INTO user(uid, username, email, password, public) VALUES(1, 'llama', 'llama@gmail.com', 'llamallamallama', 1)")
+		#cur.execute("INSERT INTO user(uid, username, email, password, public) VALUES(2, 'alpaca', 'alpaca@gmail.com', 'alpacaalpacaalpaca', 1)")
+		cur.execute("INSERT INTO trips(tid, uid, trip_name) VALUES(1,1,'llama')")
 		con.commit()
 		cur.close()
 	return render_template('home.html')
