25579cec62dfa3e4ca307137ccdd1eafadf6a85c
Eric Tay <eric.tay@duke.edu>
2019-11-04 21:36:36 -0500

    Working on Flights Form

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -15,3 +15,16 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class FlightsForm(FlaskForm):
+    fid = IntegerField('Fid')
+    tid = IntegerField('Tid')
+    airline_iata = StringField('Airline Code', [validators.Length(min=2, max=2)])
+    flight_num = IntegerField('Flight Number')
+    depart_iata = StringField('Departure Airport', [validators.Length(min=3, max=3)])
+    arrival_iata = StringField('Arrival Airport', [validators.Length(min=3, max=3)])
+    depart_datetime = DateTimeField('Departure Date & Time')
+    arrival_datetime = DateTimeField('Arrival Date & Time')
+    duration = IntegerField('Duration/hr')
+    mileage = IntegerField('Mileage')
+    submit = SubmitField('Create Flight')
