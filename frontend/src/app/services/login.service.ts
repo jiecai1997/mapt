9aab6c11b3091c3168108f5d9ad6fc9586752814
Jaiveer Katariya <jk386@duke.edu>
2019-11-05 10:27:14 -0500

    more work on login service

--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +12,45 @@ export class LoginService {
 
   constructor(private http:HttpClient) { }
 
-  private newUser(){
+  private newUser(username:string, email:string, password:string){
     // put request to make new user, make a salt-generating function to write to db
+    // write to DB salted password - see how to work with request bodies
+    
+    const hashedSalty:string = this.generatePassword(email, password);
+
+    const reqBody = {'username':username, 'email': email, 'hashedPassword':hashedSalty};
+
+    return this.http.post(this.serverURL + '/register', reqBody).subscribe( result => {
+      return result;
+    });
+    
   }
 
-  private attemptLogin():string{
-    this.http.get(this.serverURL + '/login/salt/:user').subscribe( salt => {
-      // hash password with salt in it ==> 
-      // make a post request to see if user and password + salt combo is valid
-      // result: {success: true, userid: userid} or {success: false}
+  private attemptLogin(email:string, password:string){
+    const hashedSalty:string = this.generatePassword(email, password);
+    const reqBody = {'email': email, 'hashedPassword': hashedSalty}
+
+    return this.http.post(this.serverURL + '/loginattempt', reqBody).subscribe( result => {
+      if(result['success'] == 'true'){
+        return result['uid'];
+      }
+      else{
+        return 'UNSUCCESSFUL';
+      }
+
     })
     
 
-    return 'Success';
+  }
+
+  private generatePassword(email:string, password:string):string{
+    const md5 = new Md5();
+    const salt = md5.appendStr(email).end();
+    
+    const salty = password + salt;
+    const hashed = md5.appendStr(salty);
+
+    return String(hashed);
   }
 }
 
