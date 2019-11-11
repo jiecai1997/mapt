a01a8bb5b6451f7830e3a37c18bfb6708d387f36
Ethan Holland <ethanjholland@gmail.com>
2019-11-10 23:08:44 -0500

    standardized css across profile and login pages

--- frontend/src/app/modules/material.module.ts
+++ frontend/src/app/modules/material.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {
   MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
   MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule, 
-  MatNativeDateModule, MatListModule
+  MatNativeDateModule, MatListModule, MatSlideToggleModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,6 +20,7 @@ import {
    MatIconModule,
    MatNativeDateModule,
    MatListModule,
+   MatSlideToggleModule,
    MatProgressSpinnerModule
    ],
 })
