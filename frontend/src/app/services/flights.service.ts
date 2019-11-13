d64256cf9918b147256d02390aeb9d748ae02546
Jaiveer Katariya <jk386@duke.edu>
2019-11-13 17:29:32 -0500

    deleted unused function

--- frontend/src/app/services/flights.service.ts
+++ frontend/src/app/services/flights.service.ts
@@ -16,12 +16,6 @@ export class FlightsService {
 
   getFlights():Observable<Flight[]> {
 
-    const listOfJsons = [
-      { 'flight': {'firstPoint': {'lat': 32.2226, 'long': -110.9747}, 'secondPoint': {'lat': 35.9940, 'long': -78.8986} }},
-      { 'flight': {'firstPoint': {'lat': 25.7617, 'long': -80.1918}, 'secondPoint': {'lat': 35.9940, 'long': -78.8986} }}
-    ];
-
-    const listOfFlights:Flight[] = listOfJsons.map(x => this.coordToFlight(x));
     // this.http.get(this.serverURL + '/list').subscribe( flightData => {
     //   console.log('flights', flightData);
     // });
@@ -46,26 +40,5 @@ export class FlightsService {
 
   }
 
-
-  coordToFlight(flightObj): Flight {
-    const firstPoint:Point = {
-      lat: flightObj['flight']['firstPoint']['lat'],
-      long: flightObj['flight']['firstPoint']['long']
-    }
-
-    const secondPoint:Point = {
-      lat: flightObj['flight']['secondPoint']['lat'],
-      long: flightObj['flight']['secondPoint']['long']
-    }
-
-    const myFlight:Flight = {
-      startPoint: firstPoint,
-      endPoint: secondPoint,
-      opacity: 1
-    }
-
-    return myFlight;
-
-  }
 
 }
