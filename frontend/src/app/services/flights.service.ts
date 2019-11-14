92932b03b9d97c8ae73f2b8c8927ad37e21b86fb
Jaiveer Katariya <jk386@duke.edu>
2019-11-13 22:02:58 -0500

    added some more skeletons

--- frontend/src/app/services/flights.service.ts
+++ frontend/src/app/services/flights.service.ts
@@ -4,13 +4,15 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Flight } from '../models/Flight';
 import { Point } from '../models/Point';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,
+              private loginService:LoginService) { }
 
   private serverURL = 'http://localhost:5000';
 
@@ -19,25 +21,73 @@ export class FlightsService {
     // this.http.get(this.serverURL + '/list').subscribe( flightData => {
     //   console.log('flights', flightData);
     // });
-    return this.http.get<Flight[]>(this.serverURL + '/flights')   
+    // send token as header to appease security concerns
+
+    const httpOptions = this.createAuthOptions();
+    return this.http.get<Flight[]>(this.serverURL + '/flights', httpOptions);
   }
 
   // eventually, have the backend give us a session token for the current user, and then feed that session token as well
   // as the id into the body of the request. before the request is fulfilled and the flight is added, the session token sent by
   // the frontend through the service must match the session token stored by the backend.
   // we'd need a table for that
+  // 
+  // we also need to think about color - will that get fed in from the frontend? Color string that can be sent w requestBody,
+  // have that as a column - will need that in save trip as well
   addTrip(uid:number, sessionToken:number, tripName:string, flights:Flight[]){
     const reqBody = {'userID': uid, sessionToken: sessionToken, 'tripName': tripName, 'flights': flights};
     console.log('reqBody', reqBody);
-    return this.http.post(this.serverURL + '/trips/add', reqBody).subscribe(result => {
+
+    const httpOptions = this.createAuthOptions();
+
+    return this.http.post(this.serverURL + '/trips/add', reqBody, httpOptions).subscribe(result => {
       if(result['success'] == 'true'){
         return 'Success';
       }
       else{
+        console.log(result['error']);
         return 'Add trip failed';
       }
     })
 
+  }
+
+  // anything to change here? Similar to add right?
+  modifyTrip(uid:number, sessionToken:number, tripName:string, flights:Flight[]){
+    const reqBody = {'userID': uid, sessionToken: sessionToken, 'tripName': tripName, 'flights': flights};
+    console.log('reqBody', reqBody);
+
+    const httpOptions = this.createAuthOptions();
+
+    return this.http.put(this.serverURL + '/trips/modify', reqBody, httpOptions).subscribe(result => {
+      if(result['success'] == 'true'){
+        return 'Success';
+      }
+      else{
+        return 'Modifying trip failed';
+      }
+    })
+
+  }
+
+  // anything else we need here?
+  public getStats(uid:number){
+    const httpOptions = this.createAuthOptions();
+
+    return this.http.get(this.serverURL + '/stats/?uid', httpOptions);
+  }
+
+
+  private createAuthOptions(){
+    const sessionToken = this.loginService.getToken();
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': sessionToken
+      })
+    };
+
+    return httpOptions;
   }
 
 
