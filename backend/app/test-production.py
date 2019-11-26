ae176c954fa32dfb2dc2cae3ae01d32be47975b7
Kelly Zhang <kz49@duke.edu>
2019-11-25 22:53:58 -0500

    changed formatting to be singluar names of tables as per the Models.py

--- backend/app/test-production.py
+++ backend/app/test-production.py
@@ -22,7 +22,7 @@ with sql.connect("app.db") as con:
 
     # Query 3 - Airlines whose name or abbreviation contain some substring
     cur = con.cursor()
-    cur.execute("SELECT * FROM airlines WHERE airlines.name LIKE '%Delta%' OR airlines.iata LIKE '%Delta'")
+    cur.execute("SELECT * FROM airline WHERE airline.name LIKE '%Delta%' OR airline.iata LIKE '%Delta'")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/03test.csv', index=False)
@@ -30,7 +30,7 @@ with sql.connect("app.db") as con:
 
     # Query 4 - Airports whose name or abbreviation contain some substring
     cur = con.cursor()
-    cur.execute("SELECT * FROM airports WHERE airports.name LIKE '%Dulles%' OR airports.iata LIKE '%Dulles'")
+    cur.execute("SELECT * FROM airport WHERE airport.name LIKE '%Dulles%' OR airport.iata LIKE '%Dulles'")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/04test.csv',index=False)
@@ -38,7 +38,7 @@ with sql.connect("app.db") as con:
 
     # Query 5 - Flights matching a user
     cur = con.cursor()
-    cur.execute("SELECT * FROM trips NATURAL JOIN flights WHERE uid=0")
+    cur.execute("SELECT * FROM trip NATURAL JOIN flight WHERE uid=0")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/05test.csv', index=False)
@@ -46,7 +46,7 @@ with sql.connect("app.db") as con:
 
     # Query 6 - Flights belonging to a user and matching a certain trip name
     cur = con.cursor()
-    cur.execute("SELECT * FROM trips NATURAL JOIN flights WHERE uid=1 and trip_name LIKE '%grad%'")
+    cur.execute("SELECT * FROM trip NATURAL JOIN flight WHERE uid=1 and trip_name LIKE '%grad%'")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/06test.csv', index=False)
@@ -56,7 +56,7 @@ with sql.connect("app.db") as con:
     cur = con.cursor()
     cur.execute("""
     SELECT *
-    FROM trips NATURAL JOIN flights
+    FROM trip NATURAL JOIN flight
     WHERE uid = 0
     AND depart_datetime >= '20190101 00:00:00 AM'
     AND arrival_datetime <= '20190601 11:59:59 PM'
@@ -68,7 +68,7 @@ with sql.connect("app.db") as con:
 
     # Query 8 - Total distance and duration for a given user for all trips
     cur = con.cursor()
-    cur.execute("SELECT trip_name, SUM(mileage), SUM(duration) FROM trips NATURAL JOIN flights WHERE uid=0 GROUP BY tid")
+    cur.execute("SELECT trip_name, SUM(mileage), SUM(duration) FROM trip NATURAL JOIN flight WHERE uid=0 GROUP BY tid")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/08test.csv', index=False)
@@ -78,7 +78,7 @@ with sql.connect("app.db") as con:
     cur = con.cursor()
     cur.execute("""
     SELECT SUM(mileage), SUM(duration)
-    FROM trips NATURAL JOIN flights
+    FROM trip NATURAL JOIN flights
     WHERE uid = 0
     AND depart_datetime >= '20190101 00:00:00 AM'
     AND arrival_datetime <= '20190601 11:59:59 PM'
@@ -92,13 +92,13 @@ with sql.connect("app.db") as con:
     cur = con.cursor()
     cur.execute("""
     WITH Depart_airport AS(
-    	SELECT * FROM airports
+    	SELECT * FROM airport
     ),
     Arrival_airport AS(
-    	SELECT * FROM airports
+    	SELECT * FROM airport
     )
-    SELECT flights.*
-    FROM trips NATURAL JOIN flights
+    SELECT flight.*
+    FROM trip NATURAL JOIN flights
     JOIN Depart_airport ON Depart_Airport.iata = flights.depart_iata
     JOIN Arrival_airport ON Arrival_airport.iata = flights.arrival_iata
     WHERE
@@ -119,16 +119,16 @@ with sql.connect("app.db") as con:
     cur = con.cursor()
     cur.execute("""
     WITH Depart_airport AS(
-    	SELECT * FROM airports
+    	SELECT * FROM airport
     ),
     Arrival_airport AS(
-    	SELECT * FROM airports
+    	SELECT * FROM airport
     ),
     Relevant_flights AS(
     SELECT tid
-    FROM flights
-    JOIN airports Depart_airport ON Depart_Airport.iata = flights.depart_iata
-    	JOIN airports Arrival_airport ON Arrival_airport.iata = flights.arrival_iata
+    FROM flight
+    JOIN airport Depart_airport ON Depart_Airport.iata = flight.depart_iata
+    	JOIN airport Arrival_airport ON Arrival_airport.iata = flight.arrival_iata
     WHERE
     (Depart_airport.iata LIKE '%SEA%'
     OR Depart_airport.name LIKE '%SEA%'
@@ -138,7 +138,7 @@ with sql.connect("app.db") as con:
     OR Arrival_airport.city LIKE '%SEA%')
     )
     SELECT tid, trip_name
-    FROM trips NATURAL JOIN Relevant_flights
+    FROM trip NATURAL JOIN Relevant_flights
     WHERE uid = 0
     """)
     rows = cur.fetchall()
@@ -148,7 +148,7 @@ with sql.connect("app.db") as con:
 
     #Query 12 - Details from a trip
     cur = con.cursor()
-    cur.execute('SELECT iata, note FROM trips NATURAL JOIN Details WHERE uid = 0 AND tid = 0')
+    cur.execute('SELECT iata, note FROM trip NATURAL JOIN Detail WHERE uid = 0 AND tid = 0')
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/12test.csv',index=False)
@@ -156,7 +156,7 @@ with sql.connect("app.db") as con:
 
     #Query 13 - Details from an Airport
     cur = con.cursor()
-    cur.execute("SELECT trip_name, note FROM trips NATURAL JOIN Details WHERE uid = 1 AND iata = 'IAD'")
+    cur.execute("SELECT trip_name, note FROM trip NATURAL JOIN Detail WHERE uid = 1 AND iata = 'IAD'")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/13test.csv',index=False)
@@ -164,14 +164,14 @@ with sql.connect("app.db") as con:
 
     #Query 14 - Users changing flight info
     cur = con.cursor()
-    cur.execute("SELECT * FROM flights")
+    cur.execute("SELECT * FROM flight")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/14testpart1.csv',index=False)
     cur.close()
     cur = con.cursor()
-    cur.execute("UPDATE Flights SET flight_num = 1234 WHERE fid = 0")
-    cur.execute("SELECT * FROM flights")
+    cur.execute("UPDATE Flight SET flight_num = 1234 WHERE fid = 0")
+    cur.execute("SELECT * FROM flight")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/14testpart2.csv',index=False)
@@ -179,15 +179,15 @@ with sql.connect("app.db") as con:
 
     #Query 15 - Users inserting/deleting flights from past trips
     cur = con.cursor()
-    cur.execute("DELETE FROM Flights WHERE fid = 0")
-    cur.execute("SELECT * FROM Flights")
+    cur.execute("DELETE FROM Flight WHERE fid = 0")
+    cur.execute("SELECT * FROM Flight")
     rows = cur.fetchall()
     df = DataFrame(rows)
     df.to_csv(r'./app/csv/15.csv',index=False)
-    cur.execute("DELETE FROM Flights WHERE fid = 0")
+    cur.execute("DELETE FROM Flight WHERE fid = 0")
     cur.close()
     cur = con.cursor()
-    cur.execute("INSERT INTO Flights VALUES(0, 0, 'DL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355")
+    cur.execute("INSERT INTO Flight VALUES(0, 0, 'DL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355")
     cur.close()
 
     #Query 16 - User changing email and/or password
--- backend/app/test_inserts.py
+++ backend/app/test_inserts.py
@@ -14,23 +14,23 @@ with sql.connect("app.db") as con:
     cur.execute("INSERT INTO User VALUES (0, 'sqllover', 'sqllover@duke.edu', 'password', 1)")
     cur.execute("INSERT INTO User VALUES (1, 'ra4eva', 'ra4ever@duke.edu', 'hello_password', 1)")
 
-    cur.execute("INSERT INTO Trips VALUES(0, 0, 'to internship')")
-    cur.execute("INSERT INTO Trips VALUES(1, 1, 'graduation')")
-    cur.execute("INSERT INTO Trips VALUES(2, 0, 'from internship')")
+    cur.execute("INSERT INTO Trip VALUES(0, 0, 'to internship')")
+    cur.execute("INSERT INTO Trip VALUES(1, 1, 'graduation')")
+    cur.execute("INSERT INTO Trip VALUES(2, 0, 'from internship')")
 
-    cur.execute("INSERT INTO Flights VALUES(3, 2, 'AS', 0922, 'SEA', 'BWI', '20190727 08:00:00 AM', '20190727 4:15:00 PM', 315, 2335)")
-    cur.execute("INSERT INTO Flights VALUES(2, 1, 'DL', 2220, 'IAD', 'SEA', '20190609 05:00:00 PM', '20190609 07:39:00 PM', 339, 2306)")
-    cur.execute("INSERT INTO Flights VALUES(1, 1, 'DL', 2680, 'SEA', 'IAD', '20190605 10:45:00 PM', '20190606 06:07:00 AM', 339, 2306)")
-    cur.execute("INSERT INTO Flights VALUES(0, 0, 'DL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355)")
+    cur.execute("INSERT INTO Flight VALUES(3, 2, 'AS', 0922, 'SEA', 'BWI', '20190727 08:00:00 AM', '20190727 4:15:00 PM', 315, 2335)")
+    cur.execute("INSERT INTO Flight VALUES(2, 1, 'DL', 2220, 'IAD', 'SEA', '20190609 05:00:00 PM', '20190609 07:39:00 PM', 339, 2306)")
+    cur.execute("INSERT INTO Flight VALUES(1, 1, 'DL', 2680, 'SEA', 'IAD', '20190605 10:45:00 PM', '20190606 06:07:00 AM', 339, 2306)")
+    cur.execute("INSERT INTO Flight VALUES(0, 0, 'DL', 1253, 'RDU', 'SEA', '20190505 07:25:00 AM', '20190505 10:13:00 AM', 348, 2355)")
 
-    cur.execute("INSERT INTO Details VALUES(1, 1, 'IAD', 'Best place to get boba is TeaDM')")
-    cur.execute("INSERT INTO Details VALUES(0, 1, 'IAD', 'Lots of cool museums to go to!')")
+    cur.execute("INSERT INTO Detail VALUES(1, 1, 'IAD', 'Best place to get boba is TeaDM')")
+    cur.execute("INSERT INTO Detail VALUES(0, 1, 'IAD', 'Lots of cool museums to go to!')")
 
 
-    cur.execute("INSERT INTO Airlines VALUES ('AS', 'Alaska Airlines')")
-    cur.execute("INSERT INTO Airports VALUES ('BWI','Baltimore/Washington International Thurgood Marshall Airport','Baltimore','United States',39.1754,-76.668297,'America/New_York','A')")
-    cur.execute("INSERT INTO Airports VALUES ('SEA','Seattle Tacoma International Airport','Seattle','United States',47.449001,-122.308998,'America/Los_Angeles','A')")
-    cur.execute("INSERT INTO Airports VALUES ('RDU','Raleigh Durham International Airport','Raleigh-durham','United States',35.87760162,-78.78749847,'America/New_York','A')")
+    cur.execute("INSERT INTO Airline VALUES ('AS', 'Alaska Airlines')")
+    cur.execute("INSERT INTO Airport VALUES ('BWI','Baltimore/Washington International Thurgood Marshall Airport','Baltimore','United States',39.1754,-76.668297,'America/New_York','A')")
+    cur.execute("INSERT INTO Airport VALUES ('SEA','Seattle Tacoma International Airport','Seattle','United States',47.449001,-122.308998,'America/Los_Angeles','A')")
+    cur.execute("INSERT INTO Airport VALUES ('RDU','Raleigh Durham International Airport','Raleigh-durham','United States',35.87760162,-78.78749847,'America/New_York','A')")
 
     con.commit()
     cur.close()
